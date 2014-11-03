f84325e9d453c6bd16f035f327479adfddbb3f41
hudongwen <h@ethercap.com>
2014-11-03 15:05:46 +0800

    encoding

--- misc/misc/excel.py
+++ misc/misc/excel.py
@@ -1,7 +1,7 @@
 from xlwt import Workbook
 
 def save(file_name_or_filelike_obj, title, data):
-    book = Workbook()
+    book = Workbook(encoding='utf-8')
     sheet = book.add_sheet('Sheet 1')
     for idx, val in enumerate(title):
         sheet.write(0, idx, val)
